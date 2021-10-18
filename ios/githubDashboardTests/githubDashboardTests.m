962e504b93583c04c83b1af166dd430fbdd7a1d0
Chris Glein <26607885+chrisglein@users.noreply.github.com>
2021-10-18 10:10:33 -0700

    Update android/ios namespaces

--- ios/githubDashboardTests/githubDashboardTests.m
+++ ios/githubDashboardTests/githubDashboardTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface srcTests : XCTestCase
+@interface githubDashboardTests : XCTestCase
 
 @end
 
-@implementation srcTests
+@implementation githubDashboardTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
